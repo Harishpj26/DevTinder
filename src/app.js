599b547701f763d4957439ca745500a28bddc9b3
Harish PJ <harishsmart114@gmail.com>
2026-04-09 22:29:52 +0530

    sample program to connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,11 +1,35 @@
-const express = require("express");
-const app = express();
-app.listen(7777,()=>console.log("Our server has started and running on port 7777")
-);
-app.use("/getSecretData",(req,res)=>{
-    res.send("No secreat data");
-})
-app.use("/",(req,res)=>{
-    res.send("Ready to serve the request");
-});
+const {MongoClient} = require("mongodb");
+const url = "mongodb+srv://kickstart:YNFSBxBExdnWCKsQ@sampleculster.ekneqoh.mongodb.net/";
+const client = new MongoClient(url);
+const dbName = "SampleDatabase";
 
+async function main() {
+    await client.connect();
+    console.log("Connected successfully to server");
+    const db = client.db(dbName);
+    const collection = db.collection('user');
+    const data = {
+        firstname:"Harish",
+        lastname:"KS",
+        phonenumber:"937843492348",
+        age:"21"
+    }
+    const insertResult = await collection.insertMany([data]);
+    console.log('Inserted documents =>', insertResult);
+    const updateResult = await collection.updateOne({ firstname: "Harish" }, { $set: { phonenumber: "737222CB115" } }); 
+    console.log('Updated documents =>', updateResult);
+    const findResult = await collection.find({}).toArray();
+    console.log('Found documents =>', findResult);
+    return "done";
+}
+main()
+    .then(console.log )
+    .catch(console.err)
+    .finally(()=>client.close());
+
+
+
+
+
+
+    
